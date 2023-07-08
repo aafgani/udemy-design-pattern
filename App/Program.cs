1e37c5e3e7b9a8fa53447cf45726b0dbad477b2b
aafgani <andrya.afgani@gmail.com>
2023-07-08 21:33:22 +0700

    init

---
+++ App/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {                  
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddConsole();
+                })
+                .Build()
+                .Run();
+        }
+    }
+}
